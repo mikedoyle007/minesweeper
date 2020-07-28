80b8ad2c3f3074c8e08b018a15f71e1fa777f8a0
Mike Doyle <mikedoyle007@gmail.com>
2020-07-27 22:24:56 -0700

    Fix error that caused end of game prematurely

--- app.js
+++ app.js
@@ -34,15 +34,15 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
 
+    // Add numbers
     for (let i = 0; i < squares.length; i++) {
-      // Add numbers
       let total = 0;
       const isLeftEdge = (i % width === 0);
       const isRightEdge = (i % width === width -1);
 
       if (squares[i].classList.contains('valid')) {
         if (i > 0 && !isLeftEdge && squares[i -1].classList.contains('bomb')) total++;
-        if (i > 9 && !isRightEdge && squares[i +1].classList.contains('bomb')) total++;
+        if (i > 9 && !isRightEdge && squares[i +1 -width].classList.contains('bomb')) total++;
         if (i > 10 && squares[i - width].classList.contains('bomb')) total++;
         if (i > 11 && !isLeftEdge && squares[i -1 - width].classList.contains('bomb')) total++;
         if (i < 88 && !isRightEdge && squares[i +1 + width].classList.contains('bomb')) total++;
